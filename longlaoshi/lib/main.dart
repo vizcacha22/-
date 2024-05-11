34fd773c843ea4fa6a5336f25634693a168bf0d2
Luis Calle <luedu1103@gmail.com>
2024-05-11 17:38:07 -0500

    hola: con fe noma

--- longlaoshi/lib/main.dart
+++ longlaoshi/lib/main.dart
@@ -1,10 +1,16 @@
+// The original content is temporarily commented out to allow generating a self-contained demo - feel free to uncomment later.
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'core/app_export.dart';
+import 'package:LongLaoshi/src/rust/api/simple.dart';
+import 'package:LongLaoshi/src/rust/frb_generated.dart';
 
 var globalMessengerKey = GlobalKey<ScaffoldMessengerState>();
-void main() {
+void main() async {
+  // await RustLib.init();
+  // greet(name: "Tom");
   WidgetsFlutterBinding.ensureInitialized();
   Future.wait([
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]),
